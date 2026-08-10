67c8e6d3be4f15983207a0c0657e4788eceb939b
Vadapathirasai <vadapathirasai@gmail.com>
2026-08-10 12:32:23 +0530

    Time: 34 ms (60.76%) | Memory: 60.7 MB (5.55%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,36 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
-        for(int i = 0; i < n-2; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            int l = i + 1, r = n -1;
-            while( l < r ){
-                int sum = nums[i] + nums[l] + nums[r];
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) { continue; }
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
                 if(sum == 0){
-                  ArrayList<Integer> res = new ArrayList<>();
-                  res.add(nums[i]);
-                  res.add(nums[l]);
-                  res.add(nums[r]);
-                  ans.add(res);
-                  l++;
-                  r--;
-                  while(l < r && nums[l] == nums[l-1]){
-                    l++;
-                  }
-                  while(l < r && nums[r] == nums[r+1]){
-                    r--;
-                  }
+                    ArrayList<Integer> res = new ArrayList<>();
+                    res.add(nums[i]);
+                    res.add(nums[left]);
+                    res.add(nums[right]);
+                    ans.add(res);
+                    left++; right--;
+                    while(left < right && nums[left] == nums[left-1]) { left++; }
+                    while(left < right && nums[right] == nums[right+1]) { right--; }
                 }
-                else if( sum < 0){
-                    l++;
+                else if( sum < 0 ){
+                    left++;
                 }
                 else{
-                    r--;
+                    right--;
                 }
             }
-
         }
         return ans;
     }
