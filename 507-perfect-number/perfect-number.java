23c91a9afd9fbc10eacf3d1ca173461eda80e08c
Vadapathirasai <vadapathirasai@gmail.com>
2026-08-19 21:01:00 +0530

    Time: 1 ms (95.07%) | Memory: 42.3 MB (10.14%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,9 +1,14 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum = 0;
-       for(int i = 1; i < num; i++){
+        
+        if(num == 1 || num == 0) {return false; }
+        int sum = 1;
+       for(int i = 2; i*i <= num; i++){
         if(num%i == 0){
             sum += i;
+            if( i != num/i){
+                sum += num/i;
+            }
         }
        }
        return (sum == num);
