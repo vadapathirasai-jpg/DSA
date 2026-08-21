89b3d507fb3f9fdcef1990d4c7488cf3ef070d39
Vadapathirasai <vadapathirasai@gmail.com>
2026-08-21 21:51:00 +0530

    Time: 155 ms (29.37%) | Memory: 309.7 MB (9.29%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,34 +1,27 @@
 class Solution {
+    void dfs(ArrayList<ArrayList<Integer>> list, boolean[] visited, int s){
+        visited[s] = true;
+        for(int i = 0; i < list.get(s).size(); i++){
+            int v = list.get(s).get(i);
+            if(!visited[v]){
+                dfs(list, visited, v);
+            }
+        }
+    }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
+        boolean[] visited = new boolean[n+1];
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
         for(int i = 0; i < n; i++){
             list.add(new ArrayList<>());
         }
-        for(int i = 0; i < edges.length; i++){
+        int ns = edges.length;
+        for(int i = 0; i < ns; i++){
             int u = edges[i][0];
             int v = edges[i][1];
             list.get(u).add(v);
             list.get(v).add(u);
         }
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[n+1];
-        visited[source] = true;
-        q.offer(source);
-
-        while( !q.isEmpty()){
-            int node = q.poll();
-            if(node == destination){
-                return true;
-            }
-            ArrayList<Integer> l = list.get(node);
-            for(int i = 0; i < l.size(); i++){
-                int next = l.get(i);
-                if(!visited[next]){
-                    visited[next] = true;
-                    q.offer(next);
-                }
-            }
-        }
-        return false;
+        dfs(list, visited, source);
+        return visited[destination];
     }
 }
