d9a88f006e07095269077c6d27b65dbfa04107f9
Vadapathirasai <vadapathirasai@gmail.com>
2026-09-03 23:55:16 +0530

    Time: 5 ms (83.33%) | Memory: 121.6 MB (74.64%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -3,19 +3,13 @@ class Solution {
        // Arrays.sort(nums1);
 
         int min = Integer.MAX_VALUE;
-        for(int i = 0; i < nums1.length; i++){
-            if(min > nums1[i]){
-                min = nums1[i];
+        boolean isAllEven = true;
+        for(int x : nums1){
+            min = Math.min(min, x);
+            if( x % 2 == 1){
+                isAllEven = false;
             }
         }
-        if( min%2 != 0){
-            return true;
-        }
-        for(int i = 0; i < nums1.length; i++){
-            if( (nums1[i]%2 != 0) ){
-               return false;
-            }
-        }
-        return true;
+        return isAllEven || (min % 2) == 1;
     }
 }
