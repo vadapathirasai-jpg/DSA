2d6c23453e50f53f6c984cba7dfea44e179f58af
Vadapathirasai <vadapathirasai@gmail.com>
2026-09-07 22:16:52 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (14.21%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,42 +1,49 @@
 class Solution {
-    int firstOccur(int[] arr, int k){
-        int l = 0, r = arr.length -1;
+    int firstOccur(int[] nums, int target){
+        int n = nums.length; 
+        int left = 0;
+        int right = n - 1;
         int ans = -1;
-        while( l <= r ){
-            int mid = l + ( ( r - l) >> 1);
-            if( arr[mid] == k){
-                ans = mid;
-                r = mid - 1;
-            }
-            else if(arr[mid] < k){
-               l = mid +1;
+        while( left <= right ){
+            int mid = left + ( ( right - left ) >> 1 );
+
+            if(nums[mid] < target){
+                
+                left = mid + 1;
             }
             else{
-                r = mid -1;
+                ans = mid;
+                right = mid - 1;
             }
         }
         return ans;
     }
-
-    int lastOccur(int[] arr, int k){
-        int l = 0, r = arr.length -1;
+    int lastOccur(int[] nums, int target){
+        int n = nums.length; 
+        int left = 0;
+        int right = n - 1;
         int ans = -1;
-        while( l <= r ){
-            int mid = l  + ( ( r - l ) >> 1);
-            if( arr[mid] == k){
-                ans = mid;
-                l = mid +1;
-            }
-            else if(arr[mid] > k){
-                r = mid - 1;
+        while( left <= right ){
+            int mid = left + ( ( right - left ) >> 1 );
+
+            if(nums[mid] > target){
+                
+                right = mid - 1;
             }
             else{
-                l = mid +1;
+                ans = mid;
+                left = mid + 1;
             }
         }
         return ans;
     }
     public int[] searchRange(int[] nums, int target) {
-        return new int[]{firstOccur(nums, target), lastOccur(nums, target)};
+        int x = firstOccur(nums, target);
+        if( x == -1 || nums[x] != target ){
+            return new int[]{-1, -1};
+        }
+        int y = lastOccur(nums, target);
+
+        return new int[]{x, y};
     }
 }
