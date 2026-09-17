08e9182293db4721df30caffe41052fae592434f
Vadapathirasai <vadapathirasai@gmail.com>
2026-09-17 23:17:05 +0530

    Time: 8 ms (10.55%) | Memory: 51.4 MB (85.55%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,27 +1,32 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int[][] direction = {
-            {-1,0},{1,0},{0,1},{0,-1}
-        };
         int count = 0;
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        int[][] direction = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+        };
+        int m = grid.length;
+        int n = grid[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(grid[i][j] == '1'){
-                    count++;
-                    Queue<int[]> q = new LinkedList<>();
-                    q.offer(new int[]{i, j});
                     grid[i][j] = '0';
+                  //  count++;
+                    q.add(new int[]{i, j});
                     while( !q.isEmpty() ){
                         int[] p = q.poll();
                         int x = p[0];
                         int y = p[1];
-                        for(int k = 0; k < direction.length; k++){
+                        for(int k = 0; k < 4; k++){
                             int nx = x + direction[k][0];
                             int ny = y + direction[k][1];
-                            if( nx < 0 || ny < 0 || nx >= n || ny >= m){
+                            if(nx >= m || ny >= n || nx < 0 || ny < 0 || grid[nx][ny] == '0'){
                                 continue;
                             }
                             if(grid[nx][ny] == '1'){
@@ -30,7 +35,7 @@ class Solution {
                             }
                         }
                     }
-
+                    count++;
                 }
             }
         }
